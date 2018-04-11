7d573a2f9c296829aeb11ae2d4abe40ec8425dc0
wpc1025 <1195746619@qq.com>
2018-04-11 21:39:06 +0800

    mybatis+springBoot

--- src/main/java/com/rookiego/www/wechat/dao/WeChatMessageDAO.java
+++ src/main/java/com/rookiego/www/wechat/dao/WeChatMessageDAO.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 @Mapper
 public interface WeChatMessageDAO {
-    @Insert("insert into rookie_received_message (msg_id,msg_type,msg_content,open_id,create_time) values (#{msgId},#{msgType},#{msgContent},#{openId},#{createTime})")
+    @Insert("insert into rookie_received_message (msg_id,msg_type,msg_content,open_id,create_time) " +
+            "values (#{msgId},#{msgType},#{msgContent},#{openId},#{createTime})")
     int insertReceivedMessage(Map<String, Object> map);
 }
