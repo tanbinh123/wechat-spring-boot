5328e78319a10162289de8527e5cda467cfc59f5
wpc1025 <1195746619@qq.com>
2018-04-12 18:19:14 +0800

    mybatis+springBoot

--- src/main/java/com/rookiego/www/wechat/service/impl/WeChatMessageServiceImpl.java
+++ src/main/java/com/rookiego/www/wechat/service/impl/WeChatMessageServiceImpl.java
@@ -2,6 +2,8 @@ package com.rookiego.www.wechat.service.impl;
 
 import com.rookiego.www.wechat.dao.WeChatMessageDAO;
 import com.rookiego.www.wechat.service.WeChatMessageServiceI;
+import com.rookiego.www.wechat.utils.SequenceHelper;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -21,6 +23,9 @@ public class WeChatMessageServiceImpl implements WeChatMessageServiceI {
     public int handlerReceiveMessage(String messageContent, Map<String, Object> map) {
         map.put("msgContent", messageContent);
         map.put("createTime", new Date());
+        if (StringUtils.isEmpty((String) map.get("MsgId"))) {
+            map.put("MsgId", SequenceHelper.getNextSequence());
+        }
 
         int row = weChatMessageDAO.insertReceivedMessage(map);
         LOGGER.info("save message from weChat is " + row);
